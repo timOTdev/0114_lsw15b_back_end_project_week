c2ea027029ec19931a670c0f2ffa6098b47d94ae
Timothy Hoang <timh1203@gmail.com>
2018-10-29 12:32:24 -0400

    Add /notes route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const helmet = require('helmet')
+const notesRoutes = require('./notesRoutes/notesRoutes.js');
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 
 server.get('/', (req, res) => res.send("It's Alive"))
+server.get('/notes', notesRoutes)
 
 server.listen(9000, () => console.log('\nAPI running on 9k\n'))
---
+++ notesRoutes/notesRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const knex = require('knex')
+const knexConfig = require('../knexfile.js')
+
+const router = express.Router()
+const db = knex(knexConfig.development)
+
+router.route('/')
+  .get((req, res) => {
+    db('notes')
+      .then(notes => res.status(200).json(notes))
+      .catch(err => res.status(500).json({ error: 'Could not retrieve any notes.' }))
+  })
