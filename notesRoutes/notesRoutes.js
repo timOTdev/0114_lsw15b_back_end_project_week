097b2d19f954437daf7fb3374e7c9909c1c8b358
Timothy Hoang <timh1203@gmail.com>
2018-10-29 15:17:50 -0400

    Add PUT route for /notes path

--- notesRoutes/notesRoutes.js
+++ notesRoutes/notesRoutes.js
@@ -19,4 +19,18 @@ router.route('/')
       .catch(err => res.status(500).json({ error: 'The note could not be added.' }))
   })
 
+router.route('/:id')
+  .put((req, res) => {
+    const { id } = req.params
+    const changedNote = req.body
+    db('notes')
+      .where({ id })
+      .update(changedNote)
+      .then(changedNote => {
+        if (!changedNote || changedNote < 1) return res.status(404).json({ error: 'The specific note could not be found.' })
+        return res.status(200).json(changedNote)
+      })
+      .catch(err => res.status(500).json({ error: 'The specified note could not be updated.' }))
+  })
+
 module.exports = router
