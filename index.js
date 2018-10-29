1c94bfe6a7d7cc631812ddf9c61a47b21d08f126
Timothy Hoang <timh1203@gmail.com>
2018-10-29 14:35:16 -0400

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors');
 const notesRoutes = require('./notesRoutes/notesRoutes.js');
 const server = express()
 
 server.use(helmet())
+server.use(cors());
 server.use(express.json())
 
 server.get('/', (req, res) => res.send("It's Alive"))
